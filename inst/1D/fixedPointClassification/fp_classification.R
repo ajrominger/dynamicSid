7f82b0fa70a3a02016c021939a561191d4b0561d
ajrominger <ajrominger@gmail.com>
2018-12-10 16:04:41 -0700

    script for fixed point classification

---
+++ inst/1D/fixedPointClassification/fp_classification.R
@@ -0,0 +1,17 @@
+stable <- function(x) {
+    2.5 - 0.5 * (x + 2)^2
+}
+
+unstable <- function(x) {
+    2.5 - 0.5 * (x - 2)^2
+}
+
+foostable <- function(x) {
+    -x^5
+}
+
+foounstable <- function(x) {
+    x^5
+}
+
+
